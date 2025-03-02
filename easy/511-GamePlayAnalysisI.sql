8d7428602b96ee28a8f1c2646724b12472e20282
emma <emmaabryan@gmail.com>
2025-03-02 19:37:31 +0000

    add 511 and 577 solutions

---
+++ easy/511-GamePlayAnalysisI.sql
@@ -0,0 +1,5 @@
+-- Write a solution to find the first login date for each player.
+
+-- Return the result table in any order.
+
+SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id;
